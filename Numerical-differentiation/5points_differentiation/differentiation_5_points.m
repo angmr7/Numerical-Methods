db2ba9da110568ea0cc9c83b1c39f0b7746be45d
tbltnoroot <you@example.com>
2024-04-12 13:22:38 -0600

    5 points numerical differntiation

--- Numerical-differentiation/5points_differentiation/differentiation_5_points.m
+++ Numerical-differentiation/5points_differentiation/differentiation_5_points.m
@@ -1,32 +1,34 @@
 clearvars;
-h =.5 ;
+h = .5;
 x = (1:h:10)';
 y = log(x);
 dy = zeros(size(x));
+
+
+for i=3:size(x,1)-2
+    dy(i) = (1/(12*h)) * (-y(i-2) + 8*y(i-1) - 8*y(i+1) + y(i+2));
+end
+
 dy(1)= 1/(12*h)*(-25*y(1)+48*y(2)-36*y(3)+16*y(4)-3*y(5));
+dy(2)= 1/(12*h)*(-25*y(1)+48*y(2)-36*y(3)+16*y(4)-3*y(5));
+dy(end-1) = 1/(12*h)*(-25*y(end-5)+48*y(end-4)-36*y(end-3)+16*y(end-3)-3*y(end-1));
+dy(end) = 1/(12*h)*(-25*y(end-5)+48*y(end-4)-36*y(end-3)+16*y(end-3)-3*y(end-1));
 
 figure(1);
-plot(x,y, ' or ');
+plot(x, y, 'or');
 xlabel('x');
 ylabel('y');
 
 hold on;
-%plot(x,1/x,'ob')
-plot(x,dy,'.b');
+plot(x, dy, '.b');
 hold off;
-
-for i=2:size(x,1)-1
-    dy(i)= 1/(12*h)*(-y(i-2)-8*y(i-1)+8*y(i+1)-y(i+2));
-end
-
-dy(end) = 1/(12*h)*(-25*y(end-5)+48*y(end-4)-36*y(end-3)+16*y(end-3)-3*y(end-1));
 
 yy = 1./x;
 
 figure(2);
-plot(x,yy, ' -g ');
+plot(x, yy, '-g');
 xlabel('x');
 ylabel('y');
 hold on;
-plot(x,dy,'.b');
+plot(x, yy, '.b');
 hold off;
